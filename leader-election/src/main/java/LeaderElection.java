99eb603168fed8e607eb297268dba255716d0192
Jose Escobar <jose.escobar@oracle.com>
2023-05-24 13:42:10 -0600

    S2:C9 - Leader Reelection Implementation

--- leader-election/src/main/java/LeaderElection.java
+++ leader-election/src/main/java/LeaderElection.java
@@ -4,6 +4,7 @@ import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -13,13 +14,13 @@ public class LeaderElection implements Watcher {
   private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
   private static final String ELECTION_NAMESPACE = "/election";
   private static final int SESSION_TIMEOUT = 3000;
-  private ZooKeeper zookeeper;
+  private ZooKeeper zooKeeper;
   private String currentZnodeName;
   public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
     LeaderElection leaderElection = new LeaderElection();
     leaderElection.connectToZookeeper();
     leaderElection.volunteerForLeadership();
-    leaderElection.electLeader();
+    leaderElection.reelectLeader();
     leaderElection.run();
     leaderElection.close();
     System.out.println("Disconnected from Zookeeper, exiting application ");
@@ -27,35 +28,43 @@ public class LeaderElection implements Watcher {
 
   public void volunteerForLeadership() throws KeeperException, InterruptedException {
     String znodePrefix = ELECTION_NAMESPACE + "/c_";
-    String znodeFullPath = zookeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+    String znodeFullPath = zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
     System.out.println("znode name: " + znodeFullPath);
     this.currentZnodeName = znodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
   }
 
-  public void electLeader() throws KeeperException, InterruptedException {
-    List<String> children = zookeeper.getChildren(ELECTION_NAMESPACE, false);
-    Collections.sort(children);
-    String smallestChild = children.get(0);
-
-    if (smallestChild.equals(currentZnodeName)) {
-      System.out.println("I am the leader");
-      return;
+  public void reelectLeader() throws KeeperException, InterruptedException {
+    Stat predecessorsStat = null;
+    String predecessorZnodeName = "";
+    while (predecessorsStat == null) {
+      List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+      Collections.sort(children);
+      String smallestChild = children.get(0);
+      if (smallestChild.equals(currentZnodeName)) {
+        System.out.println("I am the leader");
+        return;
+      } else {
+        System.out.println("I am not the leader");
+        int predecessorIndex = Collections.binarySearch(children, currentZnodeName)-1;
+        predecessorZnodeName = children.get(predecessorIndex);
+        predecessorsStat = zooKeeper.exists(ELECTION_NAMESPACE + "/" + predecessorZnodeName, this);
+      }
     }
-    System.out.println("I am not the leader, " + smallestChild + " is the leader");
+    System.out.println("Watching znode " + predecessorZnodeName);
   }
 
   public void run() throws InterruptedException {
-    synchronized (zookeeper) {
-      zookeeper.wait();
+    synchronized (zooKeeper) {
+      zooKeeper.wait();
     }
   }
 
   public void connectToZookeeper() throws IOException {
-    this.zookeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+    this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
   }
 
   public void close() throws InterruptedException {
-    zookeeper.close();
+    zooKeeper.close();
   }
 
   @Override
@@ -65,11 +74,21 @@ public class LeaderElection implements Watcher {
         if (watchedEvent.getState() == Event.KeeperState.SyncConnected) {
           System.out.println("Successfully Connected to ZooKeeper");
         } else {
-          synchronized (zookeeper) {
+          synchronized (zooKeeper) {
             System.out.println("Disconnected from Zookeeper event");
-            zookeeper.notifyAll();
+            zooKeeper.notifyAll();
           }
         }
+        break;
+      case NodeDeleted:
+        try {
+          reelectLeader();
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        } catch (KeeperException e) {
+          throw new RuntimeException(e);
+        }
+        break;
     }
   }
 }
